e94bd688569e97ea55def3f1c3f031ba2e435cb8
Alex <alex.pilkevych@gmail.com>
2019-04-07 15:12:25 +0300

    Etap 1 version

--- main.cpp
+++ main.cpp
@@ -3,95 +3,85 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
-//#if defined(_GLIBCXX_HAS_GTHREADS) && defined(_GLIBCXX_USE_C99_STDINT_TR1)
-
 
 using namespace std;
 
-int D = 0;
-bool EXIT = false;
+bool EXIT_FLAG = false;
 const int PHILOSOPHERS = 5;
-bool FORKS[PHILOSOPHERS*2] = {false};
-mutex mtx;
+bool FORKS[PHILOSOPHERS * 2] = { false };
+mutex cout_mutex;
 
-void task1(string t) {
-    // do stuff
-    for (int i = 0; i < 40; ++i) {
-        if (EXIT) {
-            return;
-        }
-        this_thread::sleep_for(std::chrono::milliseconds(200));
-        mtx.lock();
-        D++;
-        mtx.unlock();
-        cout << t << " " << D <<endl;
-    }
+
+void print(const string str){
+    cout_mutex.lock();
+    cout << str << endl;
+    cout_mutex.unlock();
 }
 
-void take_fork(int philosopher){
-    mtx.lock();
+void take_fork(int philosopher)
+{
     int L = philosopher * 2;
     int R = L + 1;
     FORKS[L] = true;
     FORKS[R] = true;
-    mtx.unlock();
-    cout << "Philosopher " << philosopher << " took forks " << L << " and " << R << endl;
+    print("Philosopher " + to_string(philosopher) + " took forks " + to_string(L) + " and " + to_string(R));
 }
 
-void release_fork(int philosopher){
-    mtx.lock();
+void release_fork(int philosopher)
+{
     int L = philosopher * 2;
     int R = L + 1;
     FORKS[L] = true;
     FORKS[R] = true;
-    mtx.unlock();
-    cout << "Philosopher " << philosopher << " released forks " << L << " and " << R << endl;
+    print("Philosopher " + to_string(philosopher) + " released forks " + to_string(L) + " and " + to_string(R));
 }
 
-void philosopher(int i, int eating_time, int thinking_time) {
-    // do stuff
+void philosopher(int i, int eating_time, int thinking_time)
+{
+    string philosopher = to_string(i);
     while (true) {
-        if (EXIT) {
-            cout << "Philosopher " << i << " exiting";
+        if (EXIT_FLAG) {
+            print("Philosopher " + philosopher + " exiting");
             return;
         }
-        cout << "Philosopher " << i << " started thinking for " << thinking_time << endl;
+        print("Philosopher " + philosopher + " started thinking for " + to_string(thinking_time));
         this_thread::sleep_for(std::chrono::milliseconds(thinking_time));
-        cout << "Philosopher " << i << " stopped thinking";
-        mtx.lock();
+        print("Philosopher " + philosopher + " stopped thinking");
+        if (EXIT_FLAG) {
+            print("Philosopher " + philosopher + " exiting");
+            return;
+        }
         take_fork(i);
-        cout << "Philosopher " << i << " started eating";
+        print("Philosopher " + philosopher + " started eating for " + to_string(eating_time));
         this_thread::sleep_for(std::chrono::milliseconds(eating_time));
-        cout << "Philosopher " << i << " stopped eating";
+        print("Philosopher " + philosopher + " stopped eating");
         release_fork(i);
-        mtx.unlock();
     }
 }
 
-void exit_handler() {
+void exit_handler()
+{
     while (true) {
-        int key = cin.get();
-        if (key) {
-            EXIT = true;
+        string key;
+        key = to_string(cin.get());
+        if (key == "e") {
+            cout << "Exit signal received " << key << endl;
+            EXIT_FLAG = true;
             break;
         }
     }
 }
 
-int main (int argc, char ** argv) {
-    int thinking_sleeping[PHILOSOPHERS][2] = {{200, 250}, {300, 300}, {400, 350}, {500, 100}, {600, 200}};
+int main(int argc, char** argv)
+{
+    int thinking_sleeping[PHILOSOPHERS][2] = { { 700, 700 }, { 350, 350 }, { 1000, 1000 }, { 1000, 1000 }, { 1000, 2000 } };
     thread threads[PHILOSOPHERS];
-    for (int i =0; i < PHILOSOPHERS; i++) {
+    for (int i = 0; i < PHILOSOPHERS; i++) {
         threads[i] = thread(philosopher, i, thinking_sleeping[i][0], thinking_sleeping[i][1]);
     }
-//    thread thread_1 = thread(philosopher, 0, 200, 200);
-//    thread thread_2 = thread(task1, "T2");
     exit_handler();
-    // do other stuff
-//    thread_2.join();
-//    thread_1.join();
-    for (int i =0; i < PHILOSOPHERS; i++) {
-        threads[i].join();
+    for (auto &thread : threads) {
+        thread.join();
     }
     cout << "Ended" << endl;
     return 0;
